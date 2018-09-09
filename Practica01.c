1b8b660142bd1b2d56bc241e84d833ee5a4410f9
Claudia <claugaro@ciencias.unam.mx>
2018-09-09 17:22:35 -0500

    No tengo mucha idea de lo que estoy haciendo xd

--- Practica01.c
+++ Practica01.c
@@ -11,15 +11,30 @@ int main()
 	//Se indica el inicio de la sección de código dónde se crearan los hilos y al final de esta los hilos se destruirán
 	#pragma omp parallel private(nhilos,hilo_id)
 	{
+		//Variables para Dekker
+		int turno = 0;
+		int bandera_compartida[2] = {0,0};
+
 		//Obtenemos el id del hilo en ejecución
 		hilo_id = omp_get_thread_num();
 
 		//Obtenemos el número de hilos a ejecutarse
 		nhilos = omp_get_num_threads();
 
-		//Se imprime el número de hilo que se está ejecutando
-		printf("Número de hilo en ejecución: %d\n", hilo_id);
+		//Se imprime el número de hilo que entró a la sección crítica
+		printf("El hilo %d acaba de entrar a la sección crítica \n", hilo_id);
 	
+	}
+
+
+	//Algoritmo Dekker
+	void dekker()
+	{
+		int bandera[1];
+		int otro = (hilo_id + 1)%2;
+		bandera[hilo_id] = 1; //El hilo solicita el acceso
+		while(bandera[otro]);
+
 	}
 
 }
